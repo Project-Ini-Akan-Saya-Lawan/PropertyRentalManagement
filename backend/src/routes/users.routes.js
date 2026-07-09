e4ce1a973cda32b124ab61298fc6fde76d6f6d76
amartaida <ashrafmalabali880@gmail.com>
2026-07-10 01:01:51 +0800

    add users api

---
+++ backend/src/routes/users.routes.js
@@ -0,0 +1,12 @@
+// src/routes/users.routes.js
+const express = require('express');
+const router = express.Router();
+const authenticateJWT = require('../middlewares/authenticatejwt');
+const usersController = require('../controllers/users.controller');
+
+router.get('/', authenticateJWT, usersController.getUsers);
+router.get('/me', authenticateJWT, usersController.getMyProfile);
+router.put('/me', authenticateJWT, usersController.updateMyProfile);
+router.delete('/me', authenticateJWT, usersController.deleteMyAccount);
+
+module.exports = router;
